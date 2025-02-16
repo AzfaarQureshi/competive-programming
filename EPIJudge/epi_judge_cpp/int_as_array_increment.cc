2404e49cecfbabb98ecb2fc65ef88c42b89a73b2
Azfaar Qureshi <20804975+AzfaarQureshi@users.noreply.github.com>
2025-02-15 20:47:17 -0500

    soln

--- EPIJudge/epi_judge_cpp/int_as_array_increment.cc
+++ EPIJudge/epi_judge_cpp/int_as_array_increment.cc
@@ -2,9 +2,27 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+/*
+ * 1,2,9 => 130
+ * 1,9,9 => 200
+ * 9,9,9 => 1000
+ * 1,2,3 => 124
+ */
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+  bool carry = true;
+  for (auto it = A.rbegin(); it != A.rend() && carry; ++it) {
+    *it += 1;
+    if (*it > 9) {
+      *it = 0;
+    } else {
+      carry = false;
+    }
+  }
+  if (carry) {
+    A.push_back(1);
+    std::swap(A[0], A[A.size()-1]);
+  }
+  return A;
 }
 
 int main(int argc, char* argv[]) {
