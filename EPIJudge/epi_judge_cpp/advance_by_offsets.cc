f6c26c60d47ca645143b8b0f6f59b0b6056bc228
Azfaar Qureshi <20804975+AzfaarQureshi@users.noreply.github.com>
2025-02-17 16:47:21 -0500

    6.4 can reach end of array

--- EPIJudge/epi_judge_cpp/advance_by_offsets.cc
+++ EPIJudge/epi_judge_cpp/advance_by_offsets.cc
@@ -1,10 +1,23 @@
 #include <vector>
+#include <algorithm>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+/*
+ * <3,3,1,0,2,0,1>
+ *              x
+ *
+ * <3,0,0,0,2,0,1>
+ * */
 bool CanReachEnd(const vector<int>& max_advance_steps) {
-  // TODO - you fill in here.
-  return true;
+  int64_t max_reachable_index = 0;
+  for (int64_t i = 0; i < max_advance_steps.size() && i <= max_reachable_index;
+        ++i) {
+    max_reachable_index = 
+      std::max(max_reachable_index, i + max_advance_steps[i]);
+  }
+
+  return max_reachable_index >= max_advance_steps.size() - 1;
 }
 
 int main(int argc, char* argv[]) {
